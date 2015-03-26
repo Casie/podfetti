4b3f726bdd8bfcd488e2025d6d897e84c8ca64f9
casie <casie.levine@gmail.com>
2015-03-26 19:10:23 -0400

    railified PostsController

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,41 +1,30 @@
 class PostsController < ApplicationController
 
-  get '/posts' do # INDEX
-    @posts = Post.all
-    erb :"posts/index.html"
+  def locate_redirect(post)
+    if post.podcast_id
+      redirect "/podcasts/#{post.podcast_id}"
+    elsif post.episode_id
+      episode_id = post.episode_id
+      episode = Episode.find(episode_id)
+      podcast = episode.podcast
+      redirect "/podcasts/#{podcast.id}/#{episode_id}" 
+    end
   end
 
-  get '/posts/new' do # NEW
-    @users = User.all
-    erb :"posts/new.html"
-  end
-
-  get "/posts/:id" do # SHOW
-    @post = Post.find(params[:id])
-
-    erb :"posts/show.html"
-  end
-
-  get '/posts/:id/edit' do
-    @post = Post.find(params[:id])
-
-    erb :"posts/edit.html"
-  end
-
-
-  post '/posts' do # CREATE
+  def create 
     @post = Post.create(params[:post])
-    redirect "/posts/#{@post.id}"
+    if @post.podcast_id
+      @podcast = Podcast.find(params[:post][:podcast_id])
+    elsif @post.episode_id
+      @episode = Episode.find(params[:post][:episode_id])
+    end
+    locate_redirect(@post)
   end  
 
-  patch "/posts/:id" do # UPDATE
-    @post = Post.find(params[:id])
-    @post.update(params[:post])
-    redirect "/posts/#{@post.id}"
+  def destroy
+   @post = Post.find(params[:id])
+   @post.destroy
+    locate_redirect(@post)
   end
-end
 
-# 1. Index all blogs posts
-# 2. show a blog posts
-# 3. present the user with a form to write a blog posts
-# 4. accept that forms data and create a blog post
+end  
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   get 'search/index'
   get 'results' => 'search#results', as: :results
 
-  resources :podcasts, only: :show
-  resources :episodes, only: :show
+  resources :podcasts, only: :show do
+  	resources :episodes, only: :show
+  end
   resources :users
   resources :comments	
   
