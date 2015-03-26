595b685d580cfc3427eab5ca2c58114e2dd2551f
casie <casie.levine@gmail.com>
2015-03-26 19:33:59 -0400

    railsified registrations and added css style sheets

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,15 @@ Rails.application.routes.draw do
   resources :podcasts, only: :show do
   	resources :episodes, only: :show
   end
+
   resources :users
-  resources :comments	
+  resources :posts, only: [:create, :destroy]	
+
+  post 'login' => 'registrations#login', as: :login
+  post 'logout' => 'registrations#logout', as: :logout
+  post 'newuser' => 'registrations#newuser', as: :newuser
+  get 'useraccount' => 'registrations#useraccount', as: :useraccount
+  get 'newuser' => 'registrations#newuser', as: :newuserform
   
   root 'search#index'
 end
