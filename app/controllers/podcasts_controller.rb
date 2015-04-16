3ff6fc7e3a84ba065ae6779cb543db579978e948
bshap27 <rshapiro3@gmail.com>
2015-04-16 16:21:40 -0400

    removed index route from search controller and put it in podcasts

--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -1,5 +1,17 @@
 class PodcastsController < ApplicationController
 
+  def index
+    @newuser = true if flash[:newuser]
+    @topics = Topic.all
+    @categories_array = ["Arts", "Business", "Comedy", "Education", "Hobbies", "Government", "Health", "Kids", "Music", "News", "Politics", "Religion", "Science", "Medicine", "Culture", "Sports", "TV", "Film", "Technology"]
+
+    # # devise login
+    # self.resource = resource_class.new(sign_in_params)
+    # clean_up_passwords(resource)
+    # yield resource if block_given?
+    # respond_with(resource, serialize_options(resource))
+  end
+  
   def show
     @podcast = Podcast.find(params[:id])
     @post = Post.new
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,11 +1,5 @@
 class SearchController < ApplicationController
 
-  def index
-		@newuser = true if flash[:newuser]
-    @topics = Topic.all
-    @categories_array = ["Arts", "Business", "Comedy", "Education", "Hobbies", "Government", "Health", "Kids", "Music", "News", "Politics", "Religion", "Science", "Medicine", "Culture", "Sports", "TV", "Film", "Technology"]
-	end
-
 	def results
 		@keyword = params[:searchkeyword]
 		if @keyword
