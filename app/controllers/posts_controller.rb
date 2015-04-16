0b44db795874c293708f643176f3811b59dd09f4
bshap27 <rshapiro3@gmail.com>
2015-04-16 16:31:36 -0400

    add user_id back into new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,17 +2,18 @@ class PostsController < ApplicationController
 
   def locate_redirect(post)
     if post.podcast_id
-      redirect "/podcasts/#{post.podcast_id}"
+      redirect_to podcast_path(post.podcast)
     elsif post.episode_id
-      episode_id = post.episode_id
-      episode = Episode.find(episode_id)
-      podcast = episode.podcast
-      redirect "/podcasts/#{podcast.id}/#{episode_id}" 
+      redirect_to podcast_episode_path(post.episode.podcast, post.episode)
+      # episode_id = post.episode_id
+      # episode = Episode.find(episode_id)
+      # podcast = episode.podcast
+      # redirect "/podcasts/#{podcast.id}/#{episode_id}" 
     end
   end
 
   def create 
-    @post = Post.create(params[:post])
+    @post = Post.create(post_params)
     if @post.podcast_id
       @podcast = Podcast.find(params[:post][:podcast_id])
     elsif @post.episode_id
@@ -27,4 +28,10 @@ class PostsController < ApplicationController
     locate_redirect(@post)
   end
 
-end  
+  private
+
+  def post_params
+    params.require(:post).permit(:user_id, :title, :content, :podcast_id, :episode_id)
+  end
+
+end
