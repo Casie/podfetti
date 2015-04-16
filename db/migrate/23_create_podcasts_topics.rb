3a652c6636beeb00e0ab237e1b3e1055dd054ef3
bshap27 <rshapiro3@gmail.com>
2015-04-16 15:18:39 -0400

    change migrations names to rails migrations- just timestamps

--- db/migrate/23_create_podcasts_topics.rb
+++
@@ -1,8 +0,0 @@
-class CreatePodcastsTopics < ActiveRecord::Migration
-  def change
-  	create_table :podcasts_topics do |t|
-  		t.integer :podcast_id
-  		t.integer :topic_id
-  	end
-  end
-end
--- db/migrate/27_create_user_podcasts.rb
+++
@@ -1,8 +0,0 @@
-class CreateUserPodcasts < ActiveRecord::Migration
-  def change
-  	create_table :user_podcasts do |t|
-  		t.integer :user_id
-  		t.integer :podcast_id
-  	end
-  end
-end
